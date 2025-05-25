b67788ae83eb353ac6f0dca4e6c79b0858a3c705
ARISALAB <150359240+ARISALAB@users.noreply.github.com>
2025-05-25 12:55:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ function updateBackgroundParallax() {
     const sectionHeight = highlightsSectionForParallax.offsetHeight;
 
     // !!! ΠΡΕΠΕΙ ΝΑ ΑΛΛΑΞΕΙΣ ΑΥΤΑ ΣΤΙΣ ΠΡΑΓΜΑΤΙΚΕΣ ΔΙΑΣΤΑΣΕΙΣ ΤΗΣ 2.jfif !!!
-    const originalImageWidth = 1920; // Παράδειγμα: Πλάτος της εικόνας 2.jfif
-    const originalImageHeight = 1080; // Παράδειγμα: Ύψος της εικόνας 2.jfif
+    const originalImageWidth = 2920; // Παράδειγμα: Πλάτος της εικόνας 2.jfif
+    const originalImageHeight = 2080; // Παράδειγμα: Ύψος της εικόνας 2.jfif
 
     let finalImageWidth, finalImageHeight;
 
